7a36ac091d7ab835d48a30f86d8669a9cf6541af
Alexander Black Jr <Alexander.Black@brinks.com>
2019-01-15 16:44:32 -0500

    2nd update

--- src/App.js
+++ src/App.js
@@ -1,89 +1,52 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
-import { Route } from 'react-router-dom'
-import CreateContact from './CreateContact'
-import * as ContactsAPI from './utils/ContactsAPI'
 
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-];
+
+
 
 class App extends Component {
+  state ={
+    contacts: [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+  removeContact = (contact) => {
+    this.setState( (currentState) => ({
+      contacts: currentState.contacts.filter( (c) =>{
+         return c.id !== contact.id
+      })
+
+    }))
+  };
+
   render(){
     return (
       <div>
-        <ListContacts contacts = { contacts }/>
+        <ListContacts
+          contacts = { this.state.contacts }
+          onDeleteContact = { this.removeContact }
+        />
       </div>
     )
   }
 }
 
-/*
-class App extends Component {
-  state = {
-    contacts: []
-  }
-  componentDidMount() {
-    ContactsAPI.getAll().then((contacts) => {
-      this.setState({ contacts })
-    })
-  }
-  removeContact = (contact) => {
-    this.setState((state) => ({
-      contacts: state.contacts.filter((c) => c.id !== contact.id)
-    }))
-
-    ContactsAPI.remove(contact)
-  }
-
-  createContact(contact) {
-    ContactsAPI.create(contact).then(contact => {
-      this.setState(state => ({
-        contacts: state.contacts.concat([ contact ])
-      }))
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <Route exact path='/' render={() => (
-          <ListContacts
-            onDeleteContact={this.removeContact}
-            contacts={this.state.contacts}
-          />
-        )}/>
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )}/>
-      </div>
-    )
-  }
-}
-*/
-
-
-export default App;
+export default App
